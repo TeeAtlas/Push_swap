73d7205ab7e18ee00d670d109ee5b15dd2a4a051
Tania Boterman <taboterm@c1s11d52.42wolfsburg.de>
2023-05-05 16:59:11 +0200

    operations adjusted and working

--- operations_r.c
+++ operations_r.c
@@ -6,12 +6,14 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 18:34:49 by taboterm          #+#    #+#             */
-/*   Updated: 2023/04/27 17:49:32 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/05/05 16:37:06 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+
+// first element becomes last - stack a
 void	rotate_ra(t_list **stack_a)
 {
 	t_list *last;
@@ -19,28 +21,13 @@ void	rotate_ra(t_list **stack_a)
 	last = (*stack_a);
 	if (!*stack_a || !(*stack_a)->next) //if empty or only one element bounce
 		return ;
-	while (last->next != NULL) // finding last element
-		last = last->next; // once it finds NULL reorders stack
-	last->next = *stack_a; // last is pointing to first element in stack
-	*stack_a = (*stack_a)->next; //move first element to the end
-	last->next->next = NULL; //set next pointer of new last element
+	while (last->next != NULL) // while node is not NULL
+		last = last->next; // looking for last element
+	last->next = *stack_a; // last element next pointer now points to entire stack
+	*stack_a = (*stack_a)->next; //moves first element to end by making second element new first element
+	last->next->next = NULL; //sets new last element 
+	//(which is was the first element and still points to the second) to null disconnecting it from the stack
 	write(1, "ra\n", 3);
-}
-
-void	rotate_rb(t_list **stack_b)
-{
-	t_list *last;
-
-	last = *stack_b;
-	if (!*stack_b || !(*stack_b)->next)
-		return ;
-	while (last->next != NULL) //while current element is not NULL
-		last = last->next; //keep looking for last element
-	//reorder the elements
-	last->next = *stack_b; //last->next = NULL and set to point to fist element
-	*stack_b = (*stack_b)->next; //2nd element now first moving first to end of stack
-	last->next->next = NULL; //sets mext pointer of new last element to null
-	//next next to null we disconnect first element from stack making new last elment
 }
 
 //reverse rotate bottom element to top
@@ -55,14 +42,39 @@ void	rotate_rra(t_list **stack_a)
 		return ;
 	while(current->next != NULL) //looking for second to last element
 	{
-		first = current;
-		current = current->next;
+		first = current; // when leaves list is pointing to second to last
+		current = current->next; // points to last
 	}
-	first->next = NULL; //remove last element from the list
+	first->next = NULL; // now we set pointer to last puting at end of list
 	current->next = *stack_a; // adds last element to the top of the list
-	*stack_a = current;
+	*stack_a = current; // stack_a points to top of stack
 	write(1, "rra\n", 4);
 }
+
+// first element becomes last - stack_b
+void	rotate_rb(t_list **stack_b)
+{
+	t_list *last;
+
+	last = *stack_b;
+	if (!*stack_b || !(*stack_b)->next)
+		return ;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = *stack_b;
+	*stack_b = (*stack_b)->next;
+	last->next->next = NULL;
+	write(1, "ra\n", 3);
+}
+
+// rotate top to bottom of a and b at the same time
+void	rotate_rr(t_list **stack_a, t_list **stack_b)
+{
+	rotate_ra(stack_a);
+	rotate_rb(stack_b);
+	write (1, "rr\n", 3);
+}
+
 
 //reverse rotate bottom element to top
 void	rotate_rrb(t_list **stack_b)
@@ -85,14 +97,11 @@ void	rotate_rrb(t_list **stack_b)
 	write(1, "rrb\n", 4);
 }
 
-// // rotate top to bottom of a and b at the same time
-// void	rotate_rr(t_list **stack_a, t_list **stack_b)
-// {
-	
-// }
 
-// // rotate bottom to top of a and b at the same time
-// void	rotate_rrr(t_list **stack_a, t_list **stack_b)
-// {
-	
-// }
+// rotate bottom to top of a and b at the same time
+void	rotate_rrr(t_list **stack_a, t_list **stack_b)
+{
+	rotate_rra(stack_a);
+	rotate_rrb(stack_b);
+	write(1, "rrr\n", 4);
+}
