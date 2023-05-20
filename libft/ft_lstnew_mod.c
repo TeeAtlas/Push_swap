f5b6adce838b42f42137e60cbc234cd7f7c4c5bc
Tania Boterman <taboterm@c1s11d51.42wolfsburg.de>
2023-05-20 14:17:59 +0200

    handles allocation failure

--- libft/ft_lstnew_mod.c
+++ libft/ft_lstnew_mod.c
@@ -6,21 +6,23 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 20:07:59 by taboterm          #+#    #+#             */
-/*   Updated: 2023/05/17 11:06:44 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/05/20 14:16:28 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew_mod(int value)
+// adds new node to list with index
+// mallocs space for nodes... must remember to free
+t_list	*ft_lstnew_mod(int value, int index)
 {
-	t_list	*new_element;
+	t_list	*new_node;
 
-	new_element = malloc(sizeof(t_list));
-	if (new_element)
-	{
-		new_element->val = value;
-		new_element->next = NULL;
-	}
-	return (new_element);
+	new_node = malloc(sizeof(t_list));
+	if (new_node == NULL) // handle allocation failure
+		return (NULL);
+	new_node->val = value;
+	new_node->index = index;
+	new_node->next = NULL;
+	return (new_node);
 }
