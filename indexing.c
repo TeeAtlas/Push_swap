7bdd2754b21bef93d9c93466ab1e65d536dfd73a
taboterm <taboterm@student.42wolfsburg.de>
2023-05-28 16:19:57 +0200

    funtions removed

--- indexing.c
+++ indexing.c
@@ -6,7 +6,7 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 10:47:14 by taboterm          #+#    #+#             */
-/*   Updated: 2023/05/28 15:56:28 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/05/28 16:19:12 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,51 +83,4 @@ void	index_val_swap(t_list **stack)
 		next = next->next; // set next to next node until null
 	}				
 	
-}
-
-int	min_value(t_list **stack)
-{
-	t_list	*current;
-	int		min;
-
-	current = (*stack); // declare current as pointer to stack
-	min = current->val; // set min to current value
-	while (current != NULL) // as long as the stack is not empty
-	{
-		if (current->val < min) // if current value is less than min value
-			min = current->val; // set min to current value
-		current = current->next; // set current to next node until null
-	}
-	return (min); // return min value
-}
-
-
-//the while(1) is an infinite loop that untill a specific condition is met
-// in this case the first node is the min value
-// one it's found it will break out of the loop
-void	sort_five(t_list **stack_a, t_list **stack_b)
-{
-	int		min;
-	int		current_min;
-	t_list 	*last;
-	
-	last = ft_lstlast(*stack_a); //storing pointer to last node in last
-	if (last != NULL) // if last node is not null
-		last->next = NULL; // we set the last node to NULL
-	min = min_value(stack_a); // find min value in stack_a
-	while ((*stack_a)->val != min) // while the first node is not the min value
-		rotate_ra(stack_a); // rotate stack_a
-	push_pb(stack_a, stack_b); // push first node to stack_b
-	current_min = min_value(stack_a); // find min value in stack_a
-	if (last->val == current_min) // if the first node is the min_next value
-		rotate_rra(stack_a); // rotate stack_a
-	else if (last->val != current_min) // if the first node is not the min value
-	{
-		while ((*stack_a)->val != current_min) // while the first node is not the min value
-			rotate_ra(stack_a); // rotate stack_a
-	}
-	push_pb(stack_a, stack_b); // push first node to stack_b
-	if_three(stack_a); // sort the remaining three nodes
-	push_pa(stack_a, stack_b); // push first node to stack_a
-	push_pa(stack_a, stack_b); // push first node to stack_a
 }
